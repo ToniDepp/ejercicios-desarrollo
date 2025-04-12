7c9d88502e62aef100844c1b3cc706e7c3e7aa64
ToniDepp <124863328+ToniDepp@users.noreply.github.com>
2025-04-11 19:51:16 -0600

    Cambios

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Principal from './components/Principal.vue'
+</script>
+
+<template>
+  <div>
+  </div>
+  <Principal msg="Vite + Vue" />
+</template>
+
+<style scoped>
+
+</style>
